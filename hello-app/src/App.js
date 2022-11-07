01499cd705bece784b273e0ba619bbcd2ff08513
mgreen1092 <morgan.green0209@gmail.com>
2022-11-07 16:24:49 -0500

    fianl hello-app

---
+++ hello-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Hello from './hello';
+
+function App() {
+  return (
+    <div className="App">
+      <Hello name='Morgan'/>
+    </div>
+  );
+}
+
+export default App;
---
+++ hello-app/src/hello.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Hello ({name}) {
+    // const name = 'Morgan'
+    // const {name} = props
+    return (
+        <div>
+            <h1>Hello {name}</h1>
+        </div>
+    )
+}
+export default Hello
